a0e96f847249390184f15e016131b6a855292143
Buvaneshwari Masilamani <BuvaneshwariM@softura.com>
2022-10-08 10:49:56 +0530

    finalrepo

---
+++ FinalProjectWebApplication/wwwroot/js/EmployeeCall.js
@@ -0,0 +1,21 @@
+﻿$.get("http://localhost:7222/api/Employee", function (data, status) {
+
+    let code = "";
+    for (let x in data) {
+        code += "<tr>"
+        code += "<td>" + data[x].employeeID + "</td>"
+        code += "<td>" + data[x].name + "</td>"
+        code += "<td>" + data[x].status + "</td>"
+        code += "<td>" + data[x].manager + "</td>"
+        code += "<td>" + data[x].wfm_Manager + "</td>"
+        code += "<td>" + data[x].email + "</td>"
+        code += "<td>" + data[x].experience + "</td>"
+        code += "<td>"
+        for (let y in data[x].skills)
+            code += data[x].skills[y] + "  "
+        code += "</td>"
+        code += "<td> <a href= SoftLockRequest?employee_ID =2 > RequestLock </a> </td> </tr>"
+    }
+    $('#tbody').html(code)
+})  
+
---
+++ FinalProjectWebApplication/wwwroot/js/apicall.js
@@ -0,0 +1,37 @@
+﻿$(document).ready(
+    function () {
+        $('#submit').click(function () {
+
+            var Username = $("#uname").val();
+            var Password = $("#psw").val();
+         
+            console.log(Username);
+            $.ajax({
+                url: 'http://localhost:7222/api/User/authenticate',
+                dataType: "json",
+                type: "GET",
+                contentType: "application/json",
+                data: 'username=' + Username + '&password=' + Password,
+                processData: false,
+                success: function (data, textStatus, jQxhr) {
+                    alert("User successfully authenticated")
+                    if (data.Role == "Manager") {
+                        window.location.replace('SoftLockPage')
+                    }
+                    else {
+                        window.location.replace('EmployeePage')
+                    }
+
+
+                },
+                error: function (jqXhr, textStatus, errorThrown) {
+                    alert("User successfully authenticated")
+                }
+            });
+
+
+
+        })
+
+    }
+);
